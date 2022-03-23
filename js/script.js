7f1fc966beb1172851e1a238e08598877e518055
Lucas Ribeiro <lucasribeirodesouza@live.com>
2022-03-23 11:54:23 -0300

    JavaScript atualizado, header fixado e outros ajustes

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@ const menuBtn = document.querySelector('#menu-btn')
 const closeBtn = document.querySelector('#close-btn')
 const menu = document.querySelector('#menu')
 
-menuBtn.addEventListener('click', () => {
+function openMenu() {
   menu.style.display = 'block'
   menuBtn.style.display = 'none'
   closeBtn.style.display = 'inline-block'
-})
+}
 
-closeBtn.addEventListener('click', () => {
+function closeMenu() {
   menu.style.display = 'none'
   menuBtn.style.display = 'inline-block'
   closeBtn.style.display = 'none'
+}
+
+menuBtn.addEventListener('click', () => {
+  openMenu()
+})
+
+closeBtn.addEventListener('click', () => {
+  closeMenu()
 })
 
 const navItems = menu.querySelectorAll('li')
@@ -28,6 +36,82 @@ navItems.forEach(item => {
   const link = item.querySelector('a')
   link.addEventListener('click', () => {
     changeActiveItems()
+
+    if (window.matchMedia('(max-width: 800px)').matches) {
+      closeMenu()
+    }
+
     link.classList.add('ativo')
+  })
+})
+
+const sections = document.querySelectorAll('section, main')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('#menu a[href*=' + sectionId + ']')
+        .classList.add('ativo')
+    } else {
+      document
+        .querySelector('#menu a[href*=' + sectionId + ']')
+        .classList.remove('ativo')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  activateMenuAtCurrentSection()
+})
+
+const habilidadesBtn = document.querySelector('#habilidadesBtn')
+const formacaoBtn = document.querySelector('#formacaoBtn')
+const cursosBtn = document.querySelector('#cursosBtn')
+
+const botoesSobre = document.querySelector('.sobre .botoes')
+
+const habilidadesDiv = document.querySelector('#habilidadesDiv')
+const formacaoDiv = document.querySelector('#formacaoDiv')
+const cursosDiv = document.querySelector('#cursosDiv')
+
+habilidadesBtn.addEventListener('click', () => {
+  formacaoDiv.style.display = 'none'
+  cursosDiv.style.display = 'none'
+  habilidadesDiv.style.display = 'flex'
+})
+
+formacaoBtn.addEventListener('click', () => {
+  habilidadesDiv.style.display = 'none'
+  cursosDiv.style.display = 'none'
+  formacaoDiv.style.display = 'flex'
+})
+
+cursosBtn.addEventListener('click', () => {
+  habilidadesDiv.style.display = 'none'
+  formacaoDiv.style.display = 'none'
+  cursosDiv.style.display = 'grid'
+})
+
+const botoes = botoesSobre.querySelectorAll('button')
+
+const changeActiveButtons = () => {
+  botoes.forEach(botao => {
+    botao.classList.remove('ativo')
+  })
+}
+
+botoesItems = botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+    changeActiveButtons()
+    botao.classList.add('ativo')
   })
 })
